456e870214bfc51bd2f9591abca638c9dea674ec
mmrtonmoybd <rmedha037@gmail.com>
2020-08-30 14:50:31 +0600

    adding

--- database/migrations/2020_08_30_083521_product_create_table.php
+++ database/migrations/2020_08_30_083521_product_create_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Product extends Migration
+class ProductCreateTable extends Migration
 {
     /**
      * Run the migrations.
@@ -16,23 +16,14 @@ class Product extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('titile');
-            
-        $table->bigIncrements('category_id');
             $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
-$table->double('price');
-
-$table->integer('views')->default(0);
-
-$table->integer('quantity')->default(1);
-
-$table->bigIncrements('admin_id');     
-
-$table->foreignId('admin_id')->constrained('admins')->onDelete('cascade');
+            $table->double('price');
+			$table->integer('discounds')->default(1);
+			$table->text('description');
+            $table->integer('views')->default(0);
+            $table->integer('quantity');     
+            $table->foreignId('admin_id')->constrained('admins')->onDelete('cascade');
             $table->timestamps();
-        });
-        Schema::table('products', function (Blueprint $table) {
-           
-           $table->
         });
     }
 
@@ -45,7 +36,7 @@ $table->foreignId('admin_id')->constrained('admins')->onDelete('cascade');
     {
        Schema::table('products', function (Blueprint $table) {
           $table->dropForeign('category_id');
-          //$table->dropForeign('')
+          $table->dropForeign('admin_id');
        });
         Schema::dropIfExists('products');
     }
