64d12d18c2971c7772c03e47db0bd3b45040eeae
mmrtonmoybd <rmedha037@gmail.com>
2020-10-06 15:00:36 +0600

    fixed sidebar

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -15,40 +15,29 @@
             <li><a class="treeview-item" href="{{ route('admin.category.add') }}" rel="noopener"><i class="icon fa fa-circle-o"></i> Category Add</a></li>
           </ul>
         </li>
+		<li><a class="app-menu__item @if (Route::currentRouteName() == 'admin.comment.list' || Route::currentRouteName() == 'admin.comment.add' || Route::currentRouteName() == 'admin.comment.update' || Route::currentRouteName() == 'admin.reply.add') {{ 'active' }} @endif" href="{{ route('admin.comment.list') }}"><i class="app-menu__icon fa fa-comment"></i><span class="app-menu__label">Comments</span></a>
+        </li>
         <li class="treeview"><a class="app-menu__item @if (Route::currentRouteName() == 'admin.product.list' || Route::currentRouteName() == 'admin.product.add' || Route::currentRouteName() == 'admin.product.update') {{ 'active' }} @endif" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-product-hunt"></i><span class="app-menu__label">Products</span><i class="treeview-indicator fa fa-angle-right"></i></a>
           <ul class="treeview-menu">
             <li><a class="treeview-item" href="{{ route('admin.product.list') }}"><i class="icon fa fa-circle-o"></i> Product Lists</a></li>
             <li><a class="treeview-item" href="{{ route('admin.product.add') }}" rel="noopener"><i class="icon fa fa-circle-o"></i> Product Add</a></li>
           </ul>
         </li>
-         <li><a class="app-menu__item {{ Route::currentRouteName() == 'admin.payment.list' ? 'active' : '' }}" href="{{ route('admin.payment.list') }}"><i class="app-menu__icon fa fa-credit-card"></i><span class="app-menu__label">Payments</span></a></li>
-        <li><a class="app-menu__item" href="charts.html"><i class="app-menu__icon fa fa-pie-chart"></i><span class="app-menu__label">Charts</span></a></li>
-        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Forms</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+		<li><a class="app-menu__item @if (Route::currentRouteName() == 'admin.order.list' || Route::currentRouteName() == 'admin.order.update') {{ 'active' }} @endif" href="{{ route('admin.order.list') }}"><i class="app-menu__icon fa fa-first-order"></i><span class="app-menu__label">Orders</span></a></li>
+         <li><a class="app-menu__item @if (Route::currentRouteName() == 'admin.payment.list' || Route::currentRouteName() == 'admin.payment.update') {{ 'active' }} @endif" href="{{ route('admin.payment.list') }}"><i class="app-menu__icon fa fa-credit-card"></i><span class="app-menu__label">Payments</span></a></li>
+		@can ('isAdmin')
+        <li class="treeview"><a class="app-menu__item @if (Route::currentRouteName() == 'admin.admin.list' || Route::currentRouteName() == 'admin.admin.add' || Route::currentRouteName() == 'admin.admin.update') {{ 'active' }} @endif" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-users"></i><span class="app-menu__label">Admins</span><i class="treeview-indicator fa fa-angle-right"></i></a>
           <ul class="treeview-menu">
-            <li><a class="treeview-item" href="form-components.html"><i class="icon fa fa-circle-o"></i> Form Components</a></li>
-            <li><a class="treeview-item" href="form-custom.html"><i class="icon fa fa-circle-o"></i> Custom Components</a></li>
-            <li><a class="treeview-item" href="form-samples.html"><i class="icon fa fa-circle-o"></i> Form Samples</a></li>
-            <li><a class="treeview-item" href="form-notifications.html"><i class="icon fa fa-circle-o"></i> Form Notifications</a></li>
+            <li><a class="treeview-item" href="{{ route('admin.admin.list') }}"><i class="icon fa fa-circle-o"></i> Admin Lists</a></li>
+            <li><a class="treeview-item" href="{{ route('admin.admin.add') }}"><i class="icon fa fa-circle-o"></i> Admin Add</a></li>
           </ul>
         </li>
-        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-th-list"></i><span class="app-menu__label">Tables</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+		@endcan
+        <li class="treeview"><a class="app-menu__item @if (Route::currentRouteName() == 'admin.user.list' || Route::currentRouteName() == 'admin.user.add' || Route::currentRouteName() == 'admin.user.update') {{ 'active' }} @endif" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-users"></i><span class="app-menu__label">Users</span><i class="treeview-indicator fa fa-angle-right"></i></a>
           <ul class="treeview-menu">
-            <li><a class="treeview-item" href="table-basic.html"><i class="icon fa fa-circle-o"></i> Basic Tables</a></li>
-            <li><a class="treeview-item" href="table-data-table.html"><i class="icon fa fa-circle-o"></i> Data Tables</a></li>
+            <li><a class="treeview-item" href="{{ route('admin.user.list') }}"><i class="icon fa fa-circle-o"></i> User Lists</a></li>
+            <li><a class="treeview-item" href="{{ route('admin.user.add') }}"><i class="icon fa fa-circle-o"></i> User Add</a></li>
           </ul>
         </li>
-        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-file-text"></i><span class="app-menu__label">Pages</span><i class="treeview-indicator fa fa-angle-right"></i></a>
-          <ul class="treeview-menu">
-            <li><a class="treeview-item" href="blank-page.html"><i class="icon fa fa-circle-o"></i> Blank Page</a></li>
-            <li><a class="treeview-item" href="page-login.html"><i class="icon fa fa-circle-o"></i> Login Page</a></li>
-            <li><a class="treeview-item" href="page-lockscreen.html"><i class="icon fa fa-circle-o"></i> Lockscreen Page</a></li>
-            <li><a class="treeview-item" href="page-user.html"><i class="icon fa fa-circle-o"></i> User Page</a></li>
-            <li><a class="treeview-item" href="page-invoice.html"><i class="icon fa fa-circle-o"></i> Invoice Page</a></li>
-            <li><a class="treeview-item" href="page-calendar.html"><i class="icon fa fa-circle-o"></i> Calendar Page</a></li>
-            <li><a class="treeview-item" href="page-mailbox.html"><i class="icon fa fa-circle-o"></i> Mailbox</a></li>
-            <li><a class="treeview-item" href="page-error.html"><i class="icon fa fa-circle-o"></i> Error Page</a></li>
-          </ul>
-        </li>
-        <li><a class="app-menu__item" href="docs.html"><i class="app-menu__icon fa fa-file-code-o"></i><span class="app-menu__label">Docs</span></a></li>
       </ul>
     </aside>
