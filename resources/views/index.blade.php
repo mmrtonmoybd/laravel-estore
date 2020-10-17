b3ac15b4b21c39209aba474f888b8515cc449dec
Moshiur Rahman Tonmoy <40266584+mmrtonmoybd@users.noreply.github.com>
2020-10-17 18:25:59 +0600

    Update index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -45,7 +45,7 @@
                                         <input type="hidden" value="1" id="quantity" name="quantity">
                                         <input type="hidden" name="color" value="{{ mrtattribute($product->color) }}" required>
                                         <input type="hidden" name="size" value="{{ mrtattribute($product->size) }}" required> 
-                                                <button class="btn btn-secondary btn-sm" class="tooltip-test" title="add to cart">
+                                                <button class="btn btn-secondary btn-sm tooltip-test" type="submit" title="add to cart">
                                                     <i class="fa fa-shopping-cart"></i> add to cart
                                                 </button>
                                     </form>
